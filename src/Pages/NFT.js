c0a2452f867d322aac0271162b5ac1454e01c484
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-13 16:09:10 -0400

    add axios + useEffect

---
+++ src/Pages/NFT.js
@@ -0,0 +1,30 @@
+import { useState, useEffect }from 'react'
+import Videos from '../Components/Videos'
+import axios from 'axios'
+
+export default function NFT() {
+  const [content, setContent] = useState([])
+  const URL = process.env.REACT_APP_API_URL
+
+  useEffect(()=> {
+    axios.get(`${URL}/content`)
+      .then((response) => {
+        setContent(response.data)
+      }).catch((error) => {
+        throw error
+      })
+  }, [URL])
+
+  
+  return (
+    <div>
+        <h1>What is an NFT?</h1>
+        {content.map((c, index)=> {return (
+          <div key={index}>
+            {c.reading_material}
+          </div>
+        )})}
+        <Videos />
+    </div>
+  )
+}
