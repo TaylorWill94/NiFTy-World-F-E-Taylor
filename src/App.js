a102e8a025dccb54216f394f926e75bbac524a75
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-13 16:11:39 -0400

    mvp deployment

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { Routes, Route } from 'react-router-dom'
 //Components
 
 //Pages
-// import Home from ""
-import Categories from '../Pages/Categories';
+import Home from "./Pages/Home"
+import Categories from './Pages/Categories';
+import NFT from "./Pages/NFT"
 
 
 function App() {
   return <div className="App">
     <Routes>
       <Route exact path="/"  element={<Home />}/>
-      <Route path="" element={<Categories />}/>
+      <Route path="/categories" element={<Categories />}/>
+      <Route path="/NFT" element={<NFT />}/>
     </Routes>
   </div>;
 }
