e34ab8b3ecca5f28ba174ba4907725d22cb9f501
Taylor W <taylorwilliams@pursuit.org>
2022-05-16 00:06:36 -0400

    Adding toggle button

--- src/Pages/NFT.js
+++ src/Pages/NFT.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import Videos from "../Components/Videos";
 import axios from "axios";
 
 export default function NFT() {
   const [content, setContent] = useState([]);
-  const [show, setShow] = useState("");
+  const [show, setShow] = useState(false);
   const URL = process.env.REACT_APP_API_URL;
 
   useEffect(() => {
@@ -22,10 +23,16 @@ export default function NFT() {
     <div key={index}>{c.reading_material}</div>
   ));
 
+  const showNft = () => {
+    setShow((text) => !text);
+  };
+
   return (
     <div className="nft">
       <h1 className="app-heading">NiFTY World</h1>
-      <h1 className="nft-home-btn">Home</h1>
+      <Link to="/">
+        <h1 className="nft-home-btn">Home</h1>
+      </Link>
       <br />
       <br />
       <h1 className="nft-heading">What is an NFT?</h1>
@@ -34,7 +41,12 @@ export default function NFT() {
       {/* {content.map((c, index) => {
         return <div key={index}>{c.reading_material}</div>;
       })} */}
-      {nft} ? <button>hide</button> : <button>show</button>
+      {nft} ?{" "}
+      <button onClick={showNft} type="button">
+        hide
+      </button>{" "}
+      : <button>show</button>
+      {show ? "Hi" : "Bye"}
       <Videos />
       <button className="next-btn">Next</button>
     </div>
