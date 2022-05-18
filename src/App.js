763ae5d84deb8726a2fe45377ce266950e616dda
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-18 18:33:55 -0400

    updated quiz

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
 //Components
+import NavBar from "./Components/NavBar";
 
 //Pages
-import Home from "./Pages/Home"
-import Categories from './Pages/Categories';
-import NFT from "./Pages/NFT"
-import NavBar from './Components/NavBar';
-
+import Home from "./Pages/Home";
+import Categories from "./Pages/Categories";
+import NFT from "./Pages/NFT";
+import Messages from "./Pages/Messages";
+import Quiz from "./Pages/Quiz";
 
 
 function App() {
@@ -17,9 +18,11 @@ function App() {
     <div>
       <NavBar />
       <Routes>
-        <Route exact path="/"  element={<Home />}/>
-        <Route path="/categories" element={<Categories />}/>
-        <Route path="/NFT" element={<NFT />}/>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/NFT" element={<NFT />} />
+        <Route path="/messages" element={<Messages />} />
+        <Route path="/quiz" element={<Quiz />} />
       </Routes>
     </div>
   );
---
+++ src/Pages/MessageForm.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function MessageForm() {
+  const API = process.env.REACT_APP_API_URL;
+  let navigate = useNavigate();
+
+  const addedComment = (addComment) => {
+    axios
+      .post(`${API}/comments`, addComment)
+      .then(
+        () => {
+          navigate("/messages", { replace: true });
+        },
+        (error) => console.error(error)
+      )
+      .catch((c) => console.warn("catch", c));
+  };
+
+  const [newComment, setNewComment] = useState({
+    name: "",
+    post: "",
+    date: "",
+  });
+
+  const handleText = (event) => {
+    setNewComment({ ...newComment, [event.target.id]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    // event.preventDefault();
+    addedComment(newComment);
+  };
+
+  return (
+    <div className="new-form">
+      <br />
+      <br />
+      <h1>ADD A NEW COMMENT...</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">NAME:</label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          value={newComment.name}
+          onChange={handleText}
+          placeholder="NAME ME"
+        />
+        <br />
+        <label htmlFor="post">POST:</label>
+        <br />
+        <input
+          type="text"
+          id="post"
+          value={newComment.post}
+          onChange={handleText}
+          placeholder="Type here"
+        />
+        <br />
+        <label htmlFor="date">DATE:</label>
+        <br />
+        <input
+          type="text"
+          id="date"
+          value={newComment.date}
+          onChange={handleText}
+          placeholder="0"
+        />
+        <br />
+        <input type="submit" />
+      </form>
+      <br />
+      <br />
+    </div>
+  );
+}
+
+export default MessageForm;
