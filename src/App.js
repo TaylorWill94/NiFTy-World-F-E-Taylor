b7ad3dfc746cefb434adb6d265a3466fc4e991a4
caveman49 <caveman4949@gmail.com>
2022-05-18 11:09:31 -0400

    update message board

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 //Dependencies
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 //Components
 
 //Pages
-import Home from "./Pages/Home"
-import Categories from './Pages/Categories';
-import NFT from "./Pages/NFT"
-
+import Home from "./Pages/Home";
+import Categories from "./Pages/Categories";
+import NFT from "./Pages/NFT";
+import Messages from "./Pages/Messages";
 
 function App() {
-  return <div className="App">
-    <Routes>
-      <Route exact path="/"  element={<Home />}/>
-      <Route path="/categories" element={<Categories />}/>
-      <Route path="/NFT" element={<NFT />}/>
-    </Routes>
-  </div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/NFT" element={<NFT />} />
+        <Route path="/messages" element={<Messages />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Messages.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function Messages() {
+  const [comments, setComments] = useState([]);
+  const URL = process.env.REACT_APP_API_URL;
+
+  useEffect(() => {
+    axios
+      .get(`${URL}/comments`)
+      .then((response) => {
+        setComments(response.data);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, [URL]);
+
+  return (
+    <div>
+      <h1>Please put your comment here!!</h1>
+      {comments.map((c, index) => {
+        return (
+          <div key={index}>
+            {c.name}
+            <div>{c.post}</div>;<div>{c.date}</div>;
+          </div>
+        );
+      })}
+    </div>
+  );
+}
