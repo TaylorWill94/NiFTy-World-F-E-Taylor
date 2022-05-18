6822763589a5212b1a232ffedfd58eb440448d01
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-18 10:38:36 -0400

    added NavBar & quiz table

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import { Routes, Route } from 'react-router-dom'
 import Home from "./Pages/Home"
 import Categories from './Pages/Categories';
 import NFT from "./Pages/NFT"
+import NavBar from './Components/NavBar';
+
 
 
 function App() {
   return <div className="App">
+    <NavBar />
     <Routes>
       <Route exact path="/"  element={<Home />}/>
       <Route path="/categories" element={<Categories />}/>
