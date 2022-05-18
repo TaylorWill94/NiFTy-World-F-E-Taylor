2b89db0818376f8e653a89f9e0b3552605da4402
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-05-18 18:43:36 -0400

    Add TryAgain and Correct Message Pages

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ import Categories from "./Pages/Categories";
 import NFT from "./Pages/NFT";
 import Messages from "./Pages/Messages";
 import Quiz from "./Pages/Quiz";
-
+import TryAgain from "Pages/TryAgain";
+import Correct from "Pages/Correct";
 
 function App() {
   return (
@@ -23,6 +24,8 @@ function App() {
         <Route path="/NFT" element={<NFT />} />
         <Route path="/messages" element={<Messages />} />
         <Route path="/quiz" element={<Quiz />} />
+        <Route path="/tryagain" element={<TryAgain />} />
+        <Route path="/correct" element={<Correct />} />
       </Routes>
     </div>
   );
