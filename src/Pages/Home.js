5fee2faefdcd60886e74ae6283e2194a0bbd4eee
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-05-18 10:32:12 -0400

    Home Page with Text Only

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,34 +1,18 @@
 import React from "react";
 import "./Home.css";
-import desktopHomeImage from "../desktopHomeImage.png";
+// import React, { useLayoutEffect, useState } from "react";
+// import { Container } from "react-bootstrap";
+// import desktopHomeImage from "../desktopHomeImage.png";
 
 function Home() {
   return (
-    <div className="page">
-      <div className="logo">NiFTy World</div>
-      {/* <img src={logo} className="logo" /> */}
-      <span
-        className="rightHeader"
-        style={{ width: "417px", height: "91px", left: "887px", top: "72px" }}
-      >
-        About
-      </span>
-      <span
-        className="rightHeader"
-        style={{ width: "417px", height: "91px", left: "1079px", top: "72px" }}
-      >
-        Login
-      </span>
-      <span
-        className="rightHeader"
-        style={{ width: "417px", height: "91px", left: "1300px", top: "72px" }}
-      >
-        Sign Up
-      </span>
-      {/* <div className="rightHeader">About Login Sign Up </div> */}
-      <div className="text">Learn about NFTs, Cryptocurrency and Finance!</div>
-
-      <img src={desktopHomeImage} className="peopleImage" />
+    <div ClassName="home">
+      {/* <img src={desktopHomeImage} className="HomeImage" alt="Cartoon People" /> */}
+      <div className="text">
+        <strong>
+          <h3>Learn about NFTs, Money, and Cryptocurrency!</h3>
+        </strong>{" "}
+      </div>
     </div>
   );
 }
