f7e9b5008dde3526df7d7fe1449ce2c55e0d4bf2
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-05-18 11:43:14 -0400

    Put local home page change onto dev

--- src/App.js
+++ src/App.js
@@ -5,21 +5,19 @@ import { Routes, Route } from "react-router-dom";
 //Components
 
 //Pages
-import Home from "./Pages/Home"
-import Categories from './Pages/Categories';
-import NFT from "./Pages/NFT"
-import NavBar from './Components/NavBar';
-
-
+import Home from "./Pages/Home";
+import Categories from "./Pages/Categories";
+import NFT from "./Pages/NFT";
+import NavBar from "./Components/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <NavBar />
       <Routes>
-        <Route exact path="/"  element={<Home />}/>
-        <Route path="/categories" element={<Categories />}/>
-        <Route path="/NFT" element={<NFT />}/>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/NFT" element={<NFT />} />
       </Routes>
     </div>
   );
