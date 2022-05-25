c6a34b7e37294c93b968f3504d031f257236ee93
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-25 14:55:48 -0400

    making category buttons dynamic

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import NavBar from "./Components/NavBar";
 //Pages
 import Home from "./Pages/Home";
 import Categories from "./Pages/Categories";
-import NFT from "./Pages/NFT";
+import CategoriesDetails from "./Pages/CategoriesDetails";
 import Messages from "./Pages/Messages";
 import Quiz from "./Pages/Quiz";
 import TryAgain from "./Pages/TryAgain";
@@ -21,11 +21,21 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/categories" element={<Categories />} />
-        <Route path="/NFT" element={<NFT />} />
-        <Route path="/messages" element={<Messages />} />
+        <Route path="/categories/:id" element={<CategoriesDetails />} />
+        {/* <Route path="/categories/:id/lessons" element={<Lessons />} />
+        <Route path="/categories/:id/lessons/:id" element={<LessonsDetails />} />
+        <Route path="/categories/:id/lessons/:id/questions" element={<Questions />} />
+        <Route path="/categories/:id/lessons/:id/questions/:id" element={<QuestionsDetails />} />
+        <Route path="/comments" element={<Comments />} />
+        <Route path="/comments/:id" element={<CommentsDetails />} />
+        <Route path="/comments/:id/new" element={<CommentsNewForm />} />
+        <Route path="/comments/:id/edit" element={<CommentsEditForm />} />
+        <Route path="*" element={<FourOhFour />} /> */}
+
+        {/* <Route path="/messages" element={<Messages />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/tryagain" element={<TryAgain />} />
-        <Route path="/correct" element={<Correct />} />
+        <Route path="/correct" element={<Correct />} /> */}
       </Routes>
     </div>
   );
--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -1,16 +1,36 @@
-import React from "react";
+import axios from "axios";
+import { useState, useEffect} from "react";
 import { Button, Container } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link , useParams} from "react-router-dom";
 
 function Categories() {
+  const [category, setCategory] = useState([])
+  const URL = process.env.REACT_APP_API_URL;
+  const { id } = useParams()
+
+  useEffect(()=>{
+      axios.get(`${URL}/categories/${id}`)
+        .then((response)=> {
+          setCategory(response.data)
+        })
+        .catch((error) => {
+          throw error
+        })
+
+  },[URL])
+
   return (
     <Container>
       <h1>Choose your category</h1>
-      <Link to="/categories/NFT">
-        <Button variant="primary">NFT</Button>
-      </Link>
-      <Button>Crypto</Button>
-      <Button>Finance</Button>
+      {category.map((category, index) => (
+        <Link to={`/${category.name}`}>
+          <Button key={category.name}>
+          {category}
+          </Button>
+        </Link>
+        
+
+      ))}
     </Container>
   );
 }
