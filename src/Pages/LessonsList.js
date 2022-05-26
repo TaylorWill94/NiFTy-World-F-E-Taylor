ecc2a0113b04dbc9a1e7cd4f6c1c582c466142cc
imolina212 <86175612+imolina212@users.noreply.github.com>
2022-05-26 11:29:39 -0400

    lessonList rendering all lessons by cat_id

--- src/Pages/LessonsList.js
+++ src/Pages/LessonsList.js
@@ -7,7 +7,7 @@ import {Button} from "react-bootstrap"
 
 export default function LessonList() {
   // const [content, setContent] = useState({});
-  const [lesson, setLesson] = useState({})
+  const [lessons, setLessons] = useState([])
   // const [show, setShow] = useState(true);
   // const [changeText, setChangeText] = useState("Learn");
   const URL = process.env.REACT_APP_API_URL;
@@ -17,13 +17,14 @@ export default function LessonList() {
     axios
       .get(`${URL}/lessons/${id}`)
       .then((response) => {
-        setLesson(response.data);
-        // console.log(`${URL}/lessons/${id}`)
+        setLessons(response.data);
+        console.log(lessons)
+        console.log(`${URL}/lessons`)
       })
       .catch((error) => {
         throw error;
       });
-  }, [id,URL]);
+  }, [id, URL]);
 
   // let nft = content.map((c, index) => (
   //   <div key={index}>{c.reading_material}</div>
@@ -33,78 +34,22 @@ export default function LessonList() {
   //   setShow((text) => !text);
   // };
 
-  // const changeBtnText = () => {
-  //   setChangeText(":D");
-  // };
+  let lessonList = lessons.map((lesson, i)=>(
+    <Button key={i} href={`/categories/${lesson.categories_id}/lessons/${lesson.id}`}>{lesson.lesson_title}</Button>
+  ))
 
-  // const closeBtn = () => {
-  //   setChangeText("Click");
-  // };
-
-  // const changeBtn = (changeText) => {
-  //   if (setShow === "true") {
-  //     changeText("Close");
-  //   } else {
-  //     changeText("Learn");
-  //   }
-  // };
-
+  // let lessonList = lessons.map((lesson, i)=>(
+  //   <div key={i}>{lesson.lesson_title}</div>
+  // ))
   return (
     <>
-      <Button href={`/categories/${lesson.categories_id}/lessons/${lesson.id}`}>{lesson.lesson_title}</Button>
-      <h1>{lesson.reading_material}</h1>
-      {/* <h2>{lesson.videos}</h2>
+    {lessonList}
+      {/* <Button href={`/categories/${lessons.categories_id}/lessons/${lessons.id}`}>{lessons.lesson_title}</Button> */}
+      {/* <h1>{lessons.reading_material}</h1> 
+      <h2>{lesson.videos}</h2>
       <h2>{lesson.categories_id}</h2>
       <h2>What is an NFT?</h2> */}
 
     </>
-  //   <div className="nft">
-  //     <h1 className="app-heading">NiFTY World</h1>
-  //     <Link to="/">
-  //       <h1 className="nft-home-btn">Home</h1>
-  //     </Link>
-  //     <br />
-  //     <br />
-  //     <h1 className="nft-heading">What is an NFT?</h1>
-  //     <br />
-  //     <br />
-  //     {{ nft } ? (
-  //       <button
-  //         className="nft-learn-btn"
-  //         onClick={() => {
-  //           showNft();
-  //           changeBtnText();
-  //         }}
-  //       >
-  //         {changeText}
-  //       </button>
-  //     ) : (
-  //       <button
-  //         className="nft-learn-btn"
-  //         onClick={() => {
-  //           showNft();
-  //           closeBtn();
-  //         }}
-  //       >
-  //         {changeText}
-  //       </button>
-  //     )}
-  //     <br />
-  //     <br />
-  //     {show
-  //       ? ""
-  //       : content.map((c, index) => {
-  //           return (
-  //             <div className="nft-info" key={index}>
-  //               {c.reading_material}
-  //             </div>
-  //           );
-  //         })}
-  //     <Videos />
-  //     <video className="little-lady" width="750" height="500" controls>
-  //       <source src={nftvideo} type="video/mp4" />
-  //     </video>
-  //     <button className="next-btn">Next</button>
-  //   </div>
   );
 }
