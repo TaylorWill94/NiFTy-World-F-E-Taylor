e753d96064e1902794f7840d42fabd8dd96ae383
Tashawnee Guarriello <tashawneeguarriello@pursuit.org>
2022-06-05 18:53:24 -0400

    working on nav

--- src/Pages/Categories.js
+++ src/Pages/Categories.js
@@ -1,37 +1,34 @@
 import axios from "axios";
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Button, Container, Stack } from "react-bootstrap";
-import { Link , useParams} from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function Categories() {
-  const [category, setCategory] = useState([])
+  const [category, setCategory] = useState([]);
   const URL = process.env.REACT_APP_API_URL;
-  const { id } = useParams()
+  const { id } = useParams();
 
-  useEffect(()=>{
-      axios.get(`${URL}/categories`)
-        .then((response)=> {
-          setCategory(response.data)
-        })
-        .catch((error) => {
-          throw error
-        })
-
-  },[id, URL])
+  useEffect(() => {
+    axios
+      .get(`${URL}/categories`)
+      .then((response) => {
+        setCategory(response.data);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }, [id, URL]);
 
   return (
-    <Container>
+    <Container className="background">
       <h1>Choose your category</h1>
-      <Stack className="p-5" direction ="vertical" gap="4">
+      <Stack className="p-5" direction="vertical" gap="4">
         {category.map((category, index) => (
           <Link key={index} to={`/categories/${category.id}`}>
-            <Button variant="outline-primary">
-            {category.name}
-            </Button>
+            <Button variant="outline-primary">{category.name}</Button>
           </Link>
         ))}
       </Stack>
-      
     </Container>
   );
 }
