8941ec36fbde6a3e62036ff4e1ea5840e6cc8473
Taylor W <taylorwilliams@pursuit.org>
2022-06-06 02:56:20 -0400

    Added Login page and SignUp page CSS

--- src/Pages/LogIn.js
+++ src/Pages/LogIn.js
@@ -1,17 +1,33 @@
+import "./Login.css";
+import { Container } from "react-bootstrap";
+
 function LogIn() {
   return (
-    <div>
-      <h1>Log In</h1>
-      <div className="logInForm">
-        <form>
-          <label htmlFor="username">username</label>
-          <input id="username" type="text" />
-          <label htmlFor="date">email</label>
-          <input id="email" type="text" />
-          <input type="submit" />
-        </form>
+    <Container className="login-page">
+      <div className="login-form">
+        <h1 className="login-header">Welcome back NiFTy Pal!</h1>
+        <br />
+        <div className="logInForm">
+          <form>
+            <label htmlFor="username">USERNAME:</label>
+            <br />
+            <br />
+            <input id="username" type="text" />
+            <br />
+            <br />
+            <label htmlFor="date">EMAIL:</label>
+            <br />
+            <br />
+            <input id="email" type="text" />
+            <br />
+            <br />
+            <button type="button" className="btn btn-outline-primary">
+              Log In
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </Container>
   );
 }
 
